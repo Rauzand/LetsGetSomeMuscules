51038710c13e2d6919065fc81aaa378975c3cc1e
Alexey <ritey@list.ru>
2023-04-26 01:20:31 +0800

    логин и регистрация работают, нужно поработать над дизайном

--- app/src/main/java/com/yy/letsgetsomemuscules/Activities/StartPageActivity.java
+++ app/src/main/java/com/yy/letsgetsomemuscules/Activities/StartPageActivity.java
@@ -3,7 +3,10 @@ package com.yy.letsgetsomemuscules.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.yy.letsgetsomemuscules.R;
 
@@ -16,5 +19,15 @@ public class StartPageActivity extends AppCompatActivity {
 
         //Убрал штуку сверху
         getSupportActionBar().hide();
+        Button button = findViewById(R.id.btnexercise);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartPageActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
     }
 }
