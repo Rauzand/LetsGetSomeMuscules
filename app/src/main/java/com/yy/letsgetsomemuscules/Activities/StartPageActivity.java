3be234664472afec750890fad2e3f9d251d40827
Alexey <ritey@list.ru>
2023-04-26 00:17:47 +0800

    убрал штуку сверху, запуск со StartPageActivity

--- app/src/main/java/com/yy/letsgetsomemuscules/Activities/StartPageActivity.java
+++ app/src/main/java/com/yy/letsgetsomemuscules/Activities/StartPageActivity.java
@@ -1,16 +1,20 @@
 package com.yy.letsgetsomemuscules.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 import com.yy.letsgetsomemuscules.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class StartPageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_start_page);
+
+        //Убрал штуку сверху
+        getSupportActionBar().hide();
     }
 }
