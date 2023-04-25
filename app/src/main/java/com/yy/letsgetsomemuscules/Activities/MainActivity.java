564c1f2ddfede548e7ab175e0b93a3c5bf2ae0b9
Alexey <ritey@list.ru>
2023-04-26 00:09:09 +0800

    набросок дизайна стартовой страницы

--- app/src/main/java/com/yy/letsgetsomemuscules/Activities/MainActivity.java
+++ app/src/main/java/com/yy/letsgetsomemuscules/Activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.yy.letsgetsomemuscules;
+package com.yy.letsgetsomemuscules.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.yy.letsgetsomemuscules.R;
 
 public class MainActivity extends AppCompatActivity {
 
